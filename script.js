110b929d6143ef9633c6a6b6837e3c99bd4824b4
Rasmus Jonsson <rasmusjons@gmail.com>
2019-11-17 23:16:37 +0100

    first commit

--- script.js
+++ script.js
@@ -1,130 +1,173 @@
-
-let displayer = document.querySelector("#display");
-let displayerAbove = document.querySelector("#displayAbove")
-
-let calculator = document.querySelector(".calculator");
-let allButtons = document.querySelectorAll("button");
-
-let input = "";
-let numberA = "";
-let numberB = "";
-let numberZ = "";
+let myLibrary = [];
+let list = document.createElement("ul");
+let fragment = document.createDocumentFragment();
+let table2 = document.querySelector(".table");
+let i = 0; 
 
 
-display();
+let bookA = new Book("Harry Potter 1", "JK Rowling", 100, "Yes");
+let bookB = new Book("Harry Potter 2", "JK Rowling", 200, "Yes");
+let bookC = new Book("Harry Potter 3", "JK Rowling", 300, "No");
+let bookD = new Book("Harry Potter 4", "JK Rowling", 400, "Yes");
+let bookE = new Book("Harry Potter 5", "JK Rowling", 500, "Yes");
+
+addBookToLibrary(bookA);
+addBookToLibrary(bookB);
+addBookToLibrary(bookC);
+addBookToLibrary(bookD);
+addBookToLibrary(bookE);
+
+
+displayBooks();
+
+
+
+
+//constructor 
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+
+}
+
+
+//lägger till böckerna i array 
+
+function addBookToLibrary(aBook) {
+  myLibrary.push(aBook)
+
+}
+
+
+
+//pop-up form. 
+const addForm = document.forms["formId"];
+addForm.addEventListener("submit", function (e) {
+  e.preventDefault();
+  let title = addForm.querySelector('input[name="title"]')
+  let titleValue = title.value;
+
+  let author = addForm.querySelector('input[name="author"]');
+  let authorValue = author.value;
+
+  let pages = addForm.querySelector('input[name="pages"]');
+  let pagesValue = pages.value;
+
+  let read = addForm.querySelector('input[name="read"]');
+  let readValue = read.value;
+
+        
+              //skapar lägger till boken i array 
+      let bookNew = new Book(titleValue, authorValue, pagesValue, readValue);
+      myLibrary.push(bookNew);
+
+
+            // lägger till boken i listan . 
+      let li = document.createElement("li");
+      let readButton = document.createElement("button");
+      readButton.classList.add("readButtons")
+      readButton.setAttribute("id", i); 
+      readButton.innerHTML = "Read";
+      listenerForNewButtons (readButton);
+
+      li.textContent = (`${bookNew.title} ${bookNew.author} ${bookNew.pages}`);
+      fragment.appendChild(li);
+      fragment.appendChild(readButton);
+
+      list.appendChild(fragment);
+
+
+
+
+      closeForm();
+
+
+
+});
 
 
 
 
 
-function display() {
-
-  for (let i = 0; i < allButtons.length; i++) {
-    allButtons[i].addEventListener("click", event => {
-      input = allButtons[i].value;
 
 
-      if (input === "+") {
-        operatorSigns(input);
 
-      } else if (input === "-") {
-        operatorSigns(input);
+//form pop-up
+function openForm() {
+  document.getElementById("myForm").style.display = "block";
+}
 
-      } else if (input === "x") {
-        operatorSigns(input);
-
-      } else if (input === "/") {
-        operatorSigns(input);
-
-      } else if (input === "AC") {
-        clearDisplay();
-        displayerAbove.innerHTML = ""
-        numberA = "";
-        numberB = "";
-        numberZ = "";
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+}
 
 
-      } else if (input === "=") 
-        equals();     
+
+// fixar listan med böcker
+function displayBooks() {
+
+  let render = myLibrary.map(book => `${book.title} ${book.author} ${book.pages}`);
+  
+
+  render.forEach(function (book) {
+    let readButton = document.createElement("button");
+    readButton.classList.add("readButtons");
+    readButton.setAttribute("id", i); 
+    readButton.innerHTML = "Read";
+    let li = document.createElement("li");
+    li.textContent = book;
+    fragment.appendChild(li);
+    fragment.appendChild(readButton);
+    i++; 
+  });
+
+  list.appendChild(fragment);
+  table2.appendChild(list);
+
+}
+
+//Readbutton klick-event
+let elem = document.querySelectorAll(".readButtons");
+
+elem.forEach(function (e) {
+  e.addEventListener("click", function () {
+
       
-      else {
-        (displayer.append(allButtons[i].value))
-        if (input === "AC") {
-          clearDisplay();
-        }
-        numberZ += allButtons[i].value;
-
-      }
+      let target = event.target; 
+      let id = target.id;
+      console.log(id); 
+      readStatus(id); 
+      
 
     })
-  }
+
+})
 
 
+
+
+function readStatus(i){
+  if (myLibrary[i].read === "Yes") {
+    myLibrary[i].read = "No";
+  } else myLibrary[i].read = "Yes";
+    
+  let stat = myLibrary[i].read; 
+  console.log(stat); 
 }
 
 
+function listenerForNewButtons (element){
 
-function equals(){
+  element.addEventListener("click", function () {
 
-    if (numberZ.includes("+")) {
-      numberB = parseInt(numberZ.replace("+", ""));
-      clearDisplay();
-      displayer.append(add(numberA, numberB));
+      
+    let target = event.target; 
+    let id = target.id;
+    console.log(id); 
+    readStatus(id); 
+    
 
-
-    } else if (numberZ.includes("-")) {
-      numberB = parseInt(numberZ.replace("-", ""));
-      clearDisplay();
-      displayer.append(subtract(numberA, numberB));
-
-    }
-
-    else if (numberZ.includes("x")) {
-      numberB = parseInt(numberZ.replace("x", ""));
-      clearDisplay();
-      displayer.append(multiply(numberA, numberB));
-
-    } else {
-      (numberZ.includes("/")) 
-      numberB = parseInt(numberZ.replace("/", ""));
-      clearDisplay();
-      displayer.append(divide(numberA, numberB));
-
-    }
-
-  }
-
-
-
-function operatorSigns(input){
-
-    numberA = parseInt(numberZ);
-    numberZ = input;
-    displayerAbove.append(numberA);
-    clearDisplay();
-  }
-
-
-
-function clearDisplay() {
-  displayer.innerHTML = "";
-
-}
-
-
-//math operators
-function add(numberA, numberB) {
-  return numberA + numberB;
-}
-
-function subtract(numberA, numberB) {
-  return numberA - numberB;
-}
-
-function multiply(numberA, numberB) {
-  return numberA * numberB;
-}
-
-function divide(numberA, numberB) {
-  return numberA / numberB;
+  })
 }
